e5e454491ad5a0020202674a7573ff8b3e3bd0d0
LucianosLionakis <lucianos.lionakis@ugent.be>
2020-07-27 00:13:32 +0200

    Added all translations and made small refactor to write less code

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,8 +1,10 @@
 class MenuController < ApplicationController
   def index
-    @starter = Meal.where(:mealtype => 0)
-    @next = Meal.where(:mealtype => 1)
-    @main = Meal.where(:mealtype => 2)
-    @mainfish = Meal.where(:mealtype => 3)
+    @all_meals = [
+      [  "starter", Meal.where(:mealtype => 0) ],
+      [     "next", Meal.where(:mealtype => 1) ],
+      [     "main", Meal.where(:mealtype => 2) ],
+      [ "mainfish", Meal.where(:mealtype => 3) ]
+    ]
   end
 end
