2529091b4bef6431795ec1a0591dc6601aa99f46
LucianosLionakis <lucianos.lionakis@ugent.be>
2020-07-26 22:31:36 +0200

    Almost ready with menu still need to add translations and it should be ready

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module App
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+    
+    # Permitted locales available for the application
+    I18n.available_locales = [:nl, :en]
+    
+    # Set default locale to Dutch
+    config.i18n.default_locale = :nl
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_meals(meals, prices, veg, type)
+    for index in 0..meals.length-1 do
+        meal = Meal.create(
+            {
+                mealtype: type,
+                title: meals[index],
+                vegeterian: veg[index],
+            })
+        for price in prices[index] do
+            Price.create({ amount: price, meal_id: meal.id })
+        end
+    end
+end
+
+### Starters ###
+Starter = 0
+create_meals(
+    ["tzatziki", "melitzanosalata", "rode_biet_sla",    "tarama"],
+    [     [5.0],             [5.0],           [5.0], [8.0, 15.0]],
+    [      true,              true,            true,       false],
+    Starter
+)
+
+
+### Next ###
+Next = 1
+create_meals(
+    ["pastourma", "grote_bonen", "okra", "fava", "dakos", "spinagepie", "fava_with_lakerda", "salade"],
+    [        [8],           [9],    [9],    [9],    [14],         [14],                [14],     [16]],
+    [      false,          true,   true,   true,    true,         true,               false,    false],
+    Next
+)
+
+### Main ###
+Main = 2
+create_meals(
+    ["kipfilet_trigonela", "filled_aubergine", "imam_baildi", "lam_with_spinage"],
+    [                [17],               [19],          [19],               [19]],
+    [               false,              false,          true,              false],
+    Main
+)
+
+### Main fish ###
+Main_fish = 3
+create_meals(
+    ["baby_squid", "octapus", "sepia", "dorade"],
+    [        [19],      [19],    [21],     [21]],
+    [       false,     false,   false,    false],
+    Main_fish
+)
